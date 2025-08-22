f02c55eec294fc221c1aa0372da2aac27c5c7178
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2025-08-21 20:57:14 -0500

    initialized some values and printed them.  they're so random without initializers

---
+++ 02_Fundamentals/exercises/noinit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    float tf1, tf2;
+    int ti1, ti2;
+    printf("tf1: %.5f\n", tf1);
+    printf("tf2: %.5f\n", tf2);
+    printf("ti1: %d\n", ti1);
+    printf("ti2: %d\n", ti2);
+    }
