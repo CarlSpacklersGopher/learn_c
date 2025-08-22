5790cbdab2d8a33741bfef1f4954887628376743
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2025-08-22 13:05:15 -0500

    created folder for textbook examples

---
+++ 02_Fundamentals/examples/dweight2.c
@@ -0,0 +1,44 @@
+/* Computes the dimensional weight of a 12" x 10" x 8" box */
+
+#include <stdio.h>
+/*
+int main(void)
+{
+    int height, length, width, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+
+}
+*/
+
+int main(void)
+{
+    int height, length, width, volume, weight;
+   
+    printf("Enter the height of the box: ");
+    scanf("%d", &height);
+    printf("Enter the length of the box: ");
+    scanf("%d", &length);
+    printf("Enter the width of the box: ");
+    scanf("%d", &width);
+    
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+
+}
