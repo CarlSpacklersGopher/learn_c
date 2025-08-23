5d51a780996a9999aafa57c7a5b03aedfad691fc
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2025-08-23 11:38:59 -0500

    new chapter directory generator

---
+++ new_chapter.py
@@ -0,0 +1,19 @@
+# Creates a new chapter directory structure.
+
+import sys
+import os
+
+if __name__ == '__main__':
+    chapter_num = sys.argv[1]
+    if len(chapter_num) == 1:
+        chapter_num = "0" + chapter_num
+
+    os.mkdir(chapter_num)
+    os.chdir(chapter_num)
+    os.mkdir('examples')
+    os.mkdir('examples/bin')
+    os.mkdir('exercises')
+    os.mkdir('exercises/bin')
+    os.mkdir('notes')
+    os.mkdir('projects')
+    os.mkdir('projects/bin')
