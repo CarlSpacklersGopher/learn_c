4f2cff873af9b8887e86f1ec3d711872612a1f43
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2025-10-26 08:38:37 -0500

    Program organization comments added

--- project_template.c
+++ project_template.c
@@ -10,11 +10,34 @@
  * Text description goes here
  * */
 
+/* #include directives */
 #include <stdio.h>
 
+/* #define directives */
+
+/* Type definitions */
+
+/* Global variable declarations */
+
+/* Prototypes for functions other than main */
+
+/* Definition of main */
 int main(void)
 {
 
 }
+
+/* Definition of other functions */
+
+/*********************************************************
+ * King suggests each function get its own boxed comment.
+ * Function name: Purpose of function
+ * Parameter list:
+ *      - Param 1 - purpose
+ *      - Param 2 - purpose
+ * Return value:
+ *
+ * Side effects: (modifying global variables, etc)
+ ********************************************************/
 
 
