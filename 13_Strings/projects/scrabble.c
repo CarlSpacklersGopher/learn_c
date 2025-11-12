bb42a29ecf6ada04c9fbb46860377e6bf8529ffb
Garrett Manship <garrett.d.manship@gmail.com>
2025-11-12 15:57:04 -0600

    Ch13P8 refactored scrabble calculator to use functions and can supply command line arg or it will prompt the user

---
+++ 13_Strings/projects/scrabble.c
@@ -0,0 +1,70 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King
+ *
+ * Chapter 8
+ * Project 12
+ *********************************************************/
+
+/* Computes the scrabble score of a word */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+const int scores[] = {
+                        ['A'] = 1,
+                        ['B'] = 3,
+                        ['C'] = 3,
+                        ['D'] = 2,
+                        ['E'] = 1,
+                        ['F'] = 4,
+                        ['G'] = 2,
+                        ['H'] = 4,
+                        ['I'] = 1,
+                        ['J'] = 8,
+                        ['K'] = 5,
+                        ['L'] = 1,
+                        ['M'] = 3,
+                        ['N'] = 1,
+                        ['O'] = 1,
+                        ['P'] = 3,
+                        ['Q'] = 10,
+                        ['R'] = 1,
+                        ['S'] = 1,
+                        ['T'] = 1,
+                        ['U'] = 1,
+                        ['V'] = 4,
+                        ['W'] = 4,
+                        ['X'] = 8,
+                        ['Y'] = 4,
+                        ['Z'] = 10,
+                     };
+
+int compute_scrabble_value(const char *word);
+
+int main(int argc, char *argv[])
+{
+    char word[80];
+    if (argc == 1) {
+        printf("Enter a word: ");
+        fgets(word, sizeof(word), stdin);
+    } else {
+        strcpy(word, argv[1]);
+    }
+
+    int score = compute_scrabble_value(word);
+
+    printf("Scrabble value: %d\n", score);
+    return 0;
+}
+
+int compute_scrabble_value(const char *word) {
+    int score = 0;
+    for (int i=0; word[i] != '\0'; i++) {
+        score += scores[toupper(word[i])];
+    }
+
+    return score;
+}
+
