18b07e86b3cdabcd297704922a384ebf762ee541
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2025-11-24 21:20:19 -0600

    exercises and Project 2 prep

---
+++ 15_LargePrograms/projects/template.c
@@ -0,0 +1,43 @@
+/*********************************************************
+ * C PROGRAMMING: A MODERN APPROACH, Second Edition
+ * By K. N. King
+ *
+ * Chapter NUMBER
+ * Project NUMBER
+ *********************************************************/
+
+/*
+ * Text description goes here
+ * */
+
+/* #include directives */
+#include <stdio.h>
+
+/* #define directives */
+
+/* Type definitions */
+
+/* Global variable declarations */
+
+/* Prototypes for functions other than main */
+
+/* Definition of main */
+int main(void)
+{
+
+}
+
+/* Definition of other functions */
+
+/*********************************************************
+ * King suggests each function get its own boxed comment.
+ * Function name: Purpose of function
+ * Parameter list:
+ *      - Param 1 - purpose
+ *      - Param 2 - purpose
+ * Return value:
+ *
+ * Side effects: (modifying global variables, etc)
+ ********************************************************/
+
+
